dc3e461c01a1a9dd3b0e3edea9fcb1dca6c8d4cb
Yousra <yousramahdi735@gmail.com>
2018-03-04 10:28:07 +0300

    change all

--- app/src/main/java/com/example/hp/votingsystemv1/Activities/MainActivity.java
+++ app/src/main/java/com/example/hp/votingsystemv1/Activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.hp.votingsystemv1.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,7 +7,9 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.hp.votingsystemv1.Fragments.HelpFragment;
@@ -17,11 +18,8 @@ import com.example.hp.votingsystemv1.Fragments.NoteFragment;
 import com.example.hp.votingsystemv1.Fragments.ProFragment;
 import com.example.hp.votingsystemv1.R;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    public void reference() {
-
-    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -66,14 +64,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        reference();
-
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation =findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         setTitle("Home");
@@ -82,23 +78,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FragmentTransaction fragmentTransactionTasks = getSupportFragmentManager().beginTransaction();
         fragmentTransactionTasks.replace(R.id.frame_layout, fragmentTasks);
         fragmentTransactionTasks.commit();
-    }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_profile) {
-        } else if (id == R.id.nav_notification) {
-        } else if (id == R.id.nav_setting) {
-
-        } else if (id == R.id.nav_help) {
-        } else if (id == R.id.nav_feedback) {
-        } else if (id == R.id.nav_about) {
-        } else if (id == R.id.nav_logout) {
-        }
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
